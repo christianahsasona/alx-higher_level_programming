8bbb90f8dfa87725441fecef869b17762015d52f
christianahsasona <sasonachristianah@gmail.com>
2023-09-12 19:56:42 +0000

    A function that retrieves an elent from a list like in c. Secuer access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for idx in range(len(my_list)):
+        if idx < 0:
+            return 'none'
+        elif idx > len(my_list):
+            return 'none'
+        else:
+            return my_list[idx]
